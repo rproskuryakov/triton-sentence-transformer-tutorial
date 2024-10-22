74e76d60b05d795c22868795b3926c7a5b146b21
rproskuryakov <noisyrave@gmail.com>
2024-10-22 19:11:59 +0300

    Update locustfile with triton stubs

--- locustfile.py
+++ locustfile.py
@@ -1,22 +1,36 @@
-from locust import events, task
+from locust import task
 
-# import gevent
-# import hello_pb2
-# import hello_pb2_grpc
 from src import grpc_user
-# from hello_server import start_server
+from tritonclient.grpc import service_pb2
+from tritonclient.grpc import service_pb2_grpc
 
 
-# Start the dummy server. This is not something you would do in a real test.
-# @events.init.add_listener
-# def run_grpc_server(environment, **_kwargs):
-#     gevent.spawn(start_server)
-#
-
-class HelloGrpcUser(grpc_user.GrpcUser):
-    host = "localhost:50051"
-    stub_class = hello_pb2_grpc.HelloServiceStub
+class TritonGrpcUser(grpc_user.GrpcUser):
+    host = "localhost:8001"
+    stub_class = service_pb2_grpc.GRPCInferenceServiceStub
 
     @task
-    def sayHello(self):
-        self.stub.SayHello(hello_pb2.HelloRequest(name="Test"))
+    def ensemble_request(self):
+        # model infer example
+        # https://github.com/triton-inference-server/client/blob/519124f9e1ea938efffd23b435681b5e57df9ec0/src/python/examples/grpc_client.py#L94C5-L113C47
+        self.stub.ModelInfer(
+            service_pb2.ModelInferRequest(
+                model_name="multilingual-e5-large-ensemble",
+                model_version="v3",
+                id="0",
+                inputs=[
+                    service_pb2.ModelInferRequest().InferInputTensor(
+                        name="INPUT_TEXT",
+                        datatype="TYPE_STRING",
+                        shape=[-1],
+                    ),
+                ],
+                outputs=[
+                    service_pb2.ModelInferRequest().InferRequestedOutputTensor(
+                        name="OUTPUT",
+                        datatype="FP32",
+                    )
+                ],
+                raw_input_contents=[bytes(1072812 * "a", "utf-8")]
+            )
+        )
