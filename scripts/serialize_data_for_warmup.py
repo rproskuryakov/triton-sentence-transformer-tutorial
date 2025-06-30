8fa68f374070cd43eeb1d190f6c54f7a5813c616
rproskuryakov <noisyrave@gmail.com>
2025-06-30 16:08:52 +0300

    Update project

--- scripts/serialize_data_for_warmup.py
+++ scripts/serialize_data_for_warmup.py
@@ -2,12 +2,22 @@ from transformers import AutoTokenizer
 
 
 tokenizer = AutoTokenizer.from_pretrained("intfloat/multilingual-e5-large")
-test_strings = ["hello, world!", "bye, world!"]
-batch = tokenizer(test_strings, max_length=512, padding=True, truncation=True, return_tensors='np')
+test_string = ["hello, world!",]
+batch = tokenizer(test_string, max_length=512, padding=True, truncation=True, return_tensors='np')
 print(batch["input_ids"].shape)
 print(batch["attention_mask"].shape)
-with open("models/v3/multilingual-e5-large-onnx/warmup/raw_input_ids", "wb") as fh:
+with open("models/v3/multilingual-e5-large-onnx/warmup/1/raw_input_ids", "wb") as fh:
     fh.write(batch["input_ids"])
 
-with open("models/v3/multilingual-e5-large-onnx/warmup/raw_attention_mask", "wb") as fh:
+with open("models/v3/multilingual-e5-large-onnx/warmup/1/raw_attention_mask", "wb") as fh:
+    fh.write(batch["attention_mask"])
+
+
+batch = tokenizer(test_string * 256, max_length=512, padding=True, truncation=True, return_tensors='np')
+print(batch["input_ids"].shape)
+print(batch["attention_mask"].shape)
+with open("models/v3/multilingual-e5-large-onnx/warmup/256/raw_input_ids", "wb") as fh:
+    fh.write(batch["input_ids"])
+
+with open("models/v3/multilingual-e5-large-onnx/warmup/256/raw_attention_mask", "wb") as fh:
     fh.write(batch["attention_mask"])
